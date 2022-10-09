5c6877aae4519a23f57de232f50fcd92cb7e3857
Dragomir Yordanov <dyordanov@drvy.net>
2022-10-09 23:16:44 +0200

    Add background animation

--- src/public/index.php
+++ src/public/index.php
@@ -14,7 +14,7 @@
         <header id="header" class="p-2 mb-6">
             <div class="flex justify-center align-center">
                 <h2 class="font-bold uppercase">Beardle</h2>
-                <div class="ml-2 btn btn-xs whitespace-pre cursor-default">Tipsy!</div>
+                <div class="ml-2 btn btn-xs whitespace-pre cursor-default animate-pulse">Tipsy!</div>
             </div>
         </header>
 
@@ -44,29 +44,29 @@
             </div>
         </main>
 
-        <aside id="keyboard" class="flex flex-col w-full border-t border-base-300 bg-neutral">
+        <aside id="keyboard" class="flex flex-col w-full border-t border-base-300 bg-neutral/50">
             <div id="progress" class="flex w-full p-2">
-                <div data-step-guide="0" class="flex-none border border-success h-4">
+                <div data-step-guide="0" class="flex-none border border-white/50 h-4">
                     &nbsp;
                 </div>
 
-                <div data-step-guide="1" class="flex-none border border-success h-4">
+                <div data-step-guide="1" class="flex-none border border-white/50 h-4">
                     &nbsp;
                 </div>
 
-                <div data-step-guide="2" class="flex-none border border-success h-4">
+                <div data-step-guide="2" class="flex-none border border-white/50 h-4">
                     &nbsp;
                 </div>
 
-                <div data-step-guide="3" class="flex-none border border-success h-4">
+                <div data-step-guide="3" class="flex-none border border-white/50 h-4">
                     &nbsp;
                 </div>
 
-                <div data-step-guide="4" class="flex-none border border-success h-4">
+                <div data-step-guide="4" class="flex-none border border-white/50 h-4">
                     &nbsp;
                 </div>
 
-                <div data-step-guide="5" class="flex-none border border-success h-4">
+                <div data-step-guide="5" class="flex-none border border-white/50 h-4">
                     &nbsp;
                 </div>
             </div>
@@ -74,10 +74,10 @@
             <div id="controls" class="flex justify-between items-center px-5 m-2 w-full">
                 <div id="timeCurrent">0:00</div>
 
-                <div id="control" class="p-1 border rounded-full">
-                    <div class="btn btn-sm">
-                        <svg id="playIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" /></svg>
-                    </div>
+                <div id="control">
+                    <button id="play" class="btn rounded-full border border-white/75">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9"><path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" /></svg>
+                    </button>
                 </div>
 
                 <div id="timeLength">0:00</div>
@@ -90,7 +90,7 @@
 
                 <input
                     id="answer"
-                    class="input input-bordered w-full autocomplete-input"
+                    class="input input-bordered w-full autocomplete-input bg-neutral/50"
                     type="text"
                     placeholder="What song is it?"
                     spellcheck="false"
@@ -105,6 +105,10 @@
             </div>
         </aside>
     </div>
+
+    <div id='stars'></div>
+<div id='stars2'></div>
+<div id='stars3'></div>
 <script src="assets/js/beardle.js"></script>
 <script src="assets/js/load.js"></script>
 </body>
